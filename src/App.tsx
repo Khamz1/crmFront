8cebcd9f830666cf5a03292eb6e1f58b0f874850
Adam-IT <Alan_Salons@mail.ru>
2023-08-06 16:30:58 +0300

    создал pages auth and login и добавил роуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Timer from "./Components/Timer/Timer"
 import TimerForm from "./Components/TimerForm/TimerForm"
+import Auth from "./pages/auth/auth"
+import Login from "./pages/auth/login"
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
@@ -8,6 +11,10 @@ function App() {
     <>
     <TimerForm/>
     <Timer/>
+    <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/login" element={<Login />} />
+    </Routes>
     </>
   )
 }
