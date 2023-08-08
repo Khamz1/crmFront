15736422a0215a04187713db944687c88a1c408f
Adam-IT <Alan_Salons@mail.ru>
2023-08-08 11:19:06 +0300

    доработал auth

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -3,15 +3,17 @@ import styles from "./auth.module.scss";
 import { authSignUp } from "../../features/authSlice/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../app/store";
+import { Link, useNavigate } from "react-router-dom";
 
 function Auth() {
   const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
   const authError = useSelector((state: RootState) => state.auth.error);
+  const token = useSelector((state: RootState) => state.login.token);
   const [fullName, setFullName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-  
   const handleSignUp = (e: any) => {
     e.preventDefault();
     dispatch(authSignUp({ email, password, fullName }));
@@ -19,9 +21,19 @@ function Auth() {
     setPassword("");
     setFullName("");
   };
+  React.useEffect(() => {
+    if (token) {
+      navigate("/");
+      location.reload();
+    }
+  }, [navigate, token]);
   return (
     <div className={styles.auth}>
       <form onSubmit={(e) => handleSignUp(e)} className={styles.form}>
+      <div className={styles.auth_title}>
+      <p>РЕГИСТРАЦИЯ</p>
+      <button onClick={() => navigate("/")} className={styles.btn_close}>&#10006;</button>
+      </div>
         <div className="auth_title">
           <input
             value={fullName}
@@ -46,13 +58,23 @@ function Auth() {
             placeholder="Password"
           />
         </div>
-        <button className={styles.button} role="button">Sign up</button>
+        <button className={styles.button} role="button">
+          Sign up
+        </button>
         {authError && (
-                <div style={{ color: "red" }} className={styles.auth_error}>
-                    <hr />
-                  {authError}
-                </div>
-              )}
+          <div style={{ color: "red" }} className={styles.auth_error}>
+            <hr />
+            {authError}
+          </div>
+        )}
+        <div className={styles.block_link_auth}>
+          <Link className={styles.link_login} to="/login">
+            АВТОРИЗАЦИЯ
+          </Link>
+          <Link className={styles.link_auth} to="/auth">
+            РЕГИСТРАЦИЯ
+          </Link>
+        </div>
       </form>
     </div>
   );
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -4,44 +4,75 @@ import { AppDispatch, RootState } from "../../app/store";
 import styles from "./auth.module.scss";
 import { directorDataType } from "../../types/login";
 import { fetchLogin } from "../../features/authSlice/loginSlice";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-    const dispatch: AppDispatch = useDispatch();
-  
-    const { error, token} = useSelector((state: RootState) => state.login);
+  const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
+  const { error, token } = useSelector((state: RootState) => state.login);
+console.log(token,"TOKIN");
 
-  
-    const [email, setEmail] = React.useState<string>("");
-    const [password, setPassword] = React.useState<string>("");
-  
-    const useData = { email, password } as directorDataType;
-  
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      dispatch(fetchLogin(useData));
-      setEmail("");
-      setPassword("");
-    };
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const useData = { email, password } as directorDataType;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(fetchLogin(useData));
+    setEmail("");
+    setPassword("");
+  };
+  React.useEffect(() => {
+    if (token) {
+      navigate("/");
+      location.reload();
+    }
+  }, [navigate, token]);
   return (
     <div className={styles.auth}>
-      <form   onSubmit={(e) => handleSubmit(e)} className={styles.form}>
+      
+      <form onSubmit={(e) => handleSubmit(e)} className={styles.form}>
+      <div className={styles.auth_title}>
+      <p>АВТОРИЗАЦИЯ</p>
+      <button onClick={() => navigate("/")} className={styles.btn_close}>&#10006;</button>
+      </div>
         <div className="input_title">
-          <input onChange={(e) => setEmail(e.target.value)}  value={email}  type="text" placeholder="Email" />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="text"
+            placeholder="Email"
+          />
         </div>
         <div className="input_title">
-          <input  onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder="Password" />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            placeholder="Password"
+          />
         </div>
         {/* <button className={styles.button} type="submit">Sign in</button> */}
-        <button className={styles.button} role="button">Sign in</button>
-        
+        <button className={styles.button} role="button">
+          Sign in
+        </button>
+
         {error && (
-                <div style={{ color: "red" }} className={styles.auth_error}>
-                    <hr />
-                  {error}
-                </div>
-              )}
+          <div style={{ color: "red" }} className={styles.auth_error}>
+            <hr />
+            {error}
+          </div>
+        )}
+        <div className={styles.block_link_auth}>
+          <Link className={styles.link_login} to="/login">
+            АВТОРИЗАЦИЯ
+          </Link>
+          <Link className={styles.link_auth} to="/auth">
+            РЕГИСТРАЦИЯ
+          </Link>
+        </div>
       </form>
-    
     </div>
   );
 }
