d0f68c71abe5fcf500c764cb84577ee2a3a9dc6a
Adam-IT <Alan_Salons@mail.ru>
2023-08-16 13:25:55 +0300

    исправил некоторые ошибки

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
+import Timer from "./Components/Timer/Timer";
 
-import Timer from "./Components/Timer/Timer"
-
-
-import Team from "./pages/team/team"
-import Auth from "./pages/auth/auth"
-import Login from "./pages/auth/login"
+import Team from "./pages/team/team";
+import Auth from "./pages/auth/auth";
+import Login from "./pages/auth/login";
 import { Routes, Route } from "react-router-dom";
-import EmpCreate from "./Components/Firm/EmpCreate"
-import Category from "./Components/Category/Category"
+import EmpCreate from "./Components/Firm/EmpCreate";
+import Category from "./Components/Category/Category";
 import Project from "./Components/CreateProject/Project";
-
-
+import FirmCreate from "./Components/Firm/FirmCreate";
 
 function App() {
   return (
     <>
-    <TimerForm/>
-    <Timer/>
-    <Project/>
-
-
-    <Routes>
-        <Route path="/firm" element={<FirmCreate/>}/>
-        <Route path="/emps" element={<EmpCreate/>}/>
+      <Timer />
+      <Routes>
+        <Route path="/" element={<Project />} />
+        <Route path="/firm" element={<FirmCreate />} />
+        <Route path="/emps" element={<EmpCreate />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/login" element={<Login />} />
         <Route path="/category" element={<Category />} />
-        <Route path="/team" element={<Team/>} />
-    </Routes>
- 
+        <Route path="/team" element={<Team />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
