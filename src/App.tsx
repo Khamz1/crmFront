2f9673ce9636c1cc5b999fbc3132151bf7994d81
Djamalayl <djamalayl32@mail.ru>
2023-08-16 10:45:46 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,20 @@ import Login from "./pages/auth/login"
 import { Routes, Route } from "react-router-dom";
 import Category from "./Components/Category/Category"
 import Project from "./Components/CreateProject/Project";
+import Main from "./pages/Main/Main";
 
 
 function App() {
   return (
     <>
-    <Timer/>
-    <Project/>
-
-    <Routes>
+      <Timer />
+      <Project />
+      <Main />
+      <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route path="/login" element={<Login />} />
         <Route path="/category" element={<Category />} />
-    </Routes>
+      </Routes>
 
     </>
   )
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,26 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import auth from "../features/authSlice/authSlice";
+import login from "../features/authSlice/loginSlice";
 
-import {configureStore} from '@reduxjs/toolkit'
-import auth from '../features/authSlice/authSlice'
-import login from '../features/authSlice/loginSlice'
+import firm from "../features/firmSlice/firmSlice";
+import employee from "../features/employee/employee.slice";
 
-import firm from '../features/firmSlice/firmSlice'
-import employee from '../features/employee/employee.slice'
-
-import category from '../features/categorySlice'
-import employees from '../features/employeeSlice'
-
+import category from "../features/categorySlice";
+import employees from "../features/employeeSlice";
+import projects from "../features/projectSlice"
 export const store = configureStore({
-    reducer:{
+  reducer: {
     auth,
     login,
 
     firm,
     employee,
-
+    projects,
     category,
-    employees
-
-    }
-})
+    employees,
+  },
+});
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
